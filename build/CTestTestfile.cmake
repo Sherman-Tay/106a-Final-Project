3cd223fbcf515c242788d352b6ea7faf8547dc36
David <ee106a-acf@imail.EECS.Berkeley.EDU>
2017-10-29 17:54:05 -0700

    Setup Project and automate camera start

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa17/class/ee106a-acf/ros_workspace/final_project/src
+# Build directory: /home/cc/ee106a/fa17/class/ee106a-acf/ros_workspace/final_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(camera)
+SUBDIRS(ar_track_alvar)
