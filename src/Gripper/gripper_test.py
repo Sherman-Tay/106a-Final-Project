18364bc6d4ccbde28b6df2d5f7e499245d1670d8
David <ee106a-acf@imail.EECS.Berkeley.EDU>
2017-11-17 12:46:47 -0800

    Solved camera ar tag tracking offset issue. (higher resolution)

--- src/Gripper/gripper_test.py
+++ src/Gripper/gripper_test.py
@@ -5,24 +5,25 @@ from baxter_interface import settings
 #from intera_interface import gripper as robot_gripper
 
 rospy.init_node('gripper_test')
+rospy.sleep(1.0)
 
 #Set up the right gripper
 right_gripper = robot_gripper.Gripper('right')
 
 #sets vacuum threshold at 15 out of 18
-print('Setting vacuum threshold to 18/18')
-right_gripper.set_vacuum_threshold(threshold=18)
+print('Setting vacuum threshold to 2/18')
+right_gripper.set_vacuum_threshold(threshold=2)
 
 #Initiates suction on the right gripper, setting timeout to an arbitrar
 print('Suction Activated...')
-right_gripper.command_suction(timeout=1000)
-rospy.sleep(15.0)
+right_gripper.command_suction(timeout=10.0)
+rospy.sleep(5.0)
 
 
 #Ceases suction on the right gripper
 print('Suction Deactivated...')
 right_gripper.stop()
-rospy.sleep(10.0)
+rospy.sleep(1.0)
 print('Done with cycle')
 
 #create if loop we are at start positon, init suction, only releasing when arr. final position
--- src/camera/launch/opcl_cameras.sh
+++ src/camera/launch/opcl_cameras.sh
@@ -2,4 +2,4 @@
 cd ~/ros_workspace/final_project
 rosrun baxter_tools camera_control.py -c right_hand_camera
 rosrun baxter_tools camera_control.py -o left_hand_camera -r 1280x800
-rosrun baxter_tools camera_control.py -o head_camera -r 640x400
+rosrun baxter_tools camera_control.py -o head_camera -r 1280x800
